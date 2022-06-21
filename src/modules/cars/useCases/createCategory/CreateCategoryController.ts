1194cd9998b1e600caee0dbc14c698d86f0560b8
Luiza Saquette <lu.saquette@gmail.com>
2022-06-21 16:25:59 -0300

    rentx application

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from "tsyringe";
+
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+class CreateCategoryController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, description } = request.body;
+
+        const createCategoryUsecase = container.resolve(CreateCategoryUseCase);
+
+        await createCategoryUsecase.execute({ name, description });
+
+        return response.status(201).send();
+    }
+}
+
+export { CreateCategoryController };
