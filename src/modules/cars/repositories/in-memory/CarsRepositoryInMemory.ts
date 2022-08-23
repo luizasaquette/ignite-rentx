f76d71c4a0d74cb3bccc13464f3b9f90d9461a76
Luiza Saquette <lu.saquette@gmail.com>
2022-08-23 15:35:46 -0300

    feat: create car

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,73 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+
+import { ICarsRepository } from '../ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+    cars: Car[] = [];
+
+    async create({
+        name,
+        description,
+        daily_rate,
+        license_plate,
+        fine_amount,
+        brand,
+        category_id,
+    }: ICreateCarDTO): Promise<Car> {
+        const car = new Car();
+
+        Object.assign(car, {
+            name,
+            description,
+            daily_rate,
+            license_plate,
+            fine_amount,
+            brand,
+            category_id,
+        });
+
+        this.cars.push(car);
+
+        return car;
+    }
+
+    async findByLicensePlate(license_plate: string): Promise<Car> {
+        const car = await this.cars.find(
+            car => car.license_plate === license_plate,
+        );
+
+        return car;
+    }
+
+    async findAvailable(
+        category_id?: string,
+        brand?: string,
+        name?: string,
+    ): Promise<Car[]> {
+        const cars = await this.cars.filter(car => {
+            if (
+                car.available === true ||
+                (category_id && car.category_id === category_id) ||
+                (brand && car.brand === brand) ||
+                (name && car.name === name)
+            ) {
+                return car;
+            }
+            return null;
+        });
+
+        return cars;
+    }
+
+    async findById(id: string): Promise<Car> {
+        return this.cars.find(car => car.id === id);
+    }
+
+    async updateAvailable(id: string, available: boolean): Promise<void> {
+        const index = this.cars.findIndex(car => car.id === id);
+        this.cars[index].available = available;
+    }
+}
+
+export { CarsRepositoryInMemory };
