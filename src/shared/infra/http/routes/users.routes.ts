b009051377f1122079c4f6ae94bc049c7b5413d7
Luiza Saquette <lu.saquette@gmail.com>
2022-08-23 15:37:05 -0300

    feat: create user

---
+++ src/shared/infra/http/routes/users.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '@config/upload';
+import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/updateUserAvatarController';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+
+const usersRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
+const createUserController = new CreateUserController();
+const updateUserAvataController = new UpdateUserAvatarController();
+
+usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+    '/avatar',
+    ensureAuthenticated,
+    uploadAvatar.single('avatar'),
+    updateUserAvataController.handle,
+);
+
+export { usersRoutes };
