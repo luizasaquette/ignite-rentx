b7afa96519c3453920b16f9ce71bdd3628e2ad37
Luiza Saquette <lu.saquette@gmail.com>
2022-10-26 16:38:26 -0300

    chore: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,6 +11,8 @@ import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
 import createConnection from '@shared/infra/typeorm';
 
+import cors from 'cors';
+
 import swaggerFile from '../../../swagger.json';
 
 createConnection();
@@ -23,6 +25,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
